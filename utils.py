e69f949fabb3fc2fce1d949c449b6a7b46b094f0
Ali H <sonorligh@gmail.com>
2018-01-22 19:36:31 +0100

    Progress Final

--- utils.py
+++ utils.py
@@ -5,3 +5,6 @@ def load_text(path, encoding = "utf8"):
     with codecs.open(os.path.join(paths[0], *paths[1:]),encoding=encoding) as f:
         text = f.read()
     return text
+
+def inverse_dict(d):
+    return {e[1]:e[0] for e in d.items()}
